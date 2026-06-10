eb722783249fe891e934562c06f0caa27e33fc0c
meh2023 <mehbo@users.noreply.github.com>
2026-06-11 00:14:50 +0530

    Change default local testing port to 5001 to avoid conflicts

--- flask_app.py
+++ flask_app.py
@@ -30,4 +30,4 @@ def index():
     return render_template("index.html", news_text=news_text, result=prediction_result)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
